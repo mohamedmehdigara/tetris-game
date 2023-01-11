09d252949368fe1b622d031f7d05834de94025e8
mohamedmehdigara <garamehdi57@yahoo.com>
2023-01-11 08:33:02 +0100

    timer created

--- src/components/GridBoard.js
+++ src/components/GridBoard.js
@@ -1,11 +1,34 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
-import GridSquare from './GridSquare'
-import { shapes } from '../utils'
+import React, { useEffect, useRef } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { moveDown } from '../actions'
 
 export default function GridBoard(props) {
+  useEffect(() => {
+    requestRef.current = requestAnimationFrame(update)
+    return () => cancelAnimationFrame(requestRef.current)
+}, [isRunning])
     const game = useSelector((state) => state.game)
     const { grid, shape, rotation, x, y, isRunning, speed } = game
+    const requestRef = useRef()
+    const lastUpdateTimeRef = useRef(0)
+    const progressTimeRef = useRef(0)
+  const dispatch = useDispatch()
+  const update = (time) => {
+    requestRef.current = requestAnimationFrame(update)
+    if (!isRunning) {
+        return 
+    }
+    if (!lastUpdateTimeRef.current) {
+        lastUpdateTimeRef.current = time
+    }
+    const deltaTime = time - lastUpdateTimeRef.current
+    progressTimeRef.current += deltaTime
+    if (progressTimeRef.current > speed) {
+        dispatch(moveDown())
+        progressTimeRef.current = 0
+    }
+    lastUpdateTimeRef.current = time
+} 
 
     const block = shapes[shape][rotation]
   const blockColor = shape
