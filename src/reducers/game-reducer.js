91aba628f1407cf1db2bb0d21d1c0c1d4f00b28d
mohamedmehdigara <garamehdi57@yahoo.com>
2023-01-11 09:01:18 +0100

    added files

--- src/reducers/game-reducer.js
+++ src/reducers/game-reducer.js
@@ -7,9 +7,9 @@ import {
     defaultState,
     nextRotation,
     canMoveTo,
+
     addBlockToGrid,
     checkRows,
-    randomShape
   } from '../utils'
 
   
@@ -17,9 +17,13 @@ import {
     const { shape, grid, x, y, rotation, nextShape, score, isRunning } = state
   
     switch(action.type) {
-      case ROTATE:
-  
-        return state
+      
+        case ROTATE:
+          const newRotation = nextRotation(shape, rotation)
+          if (canMoveTo(shape, grid, x, y, newRotation)) {
+              return { ...state, rotation: newRotation }
+          }
+          return state
   
         case MOVE_RIGHT:
           if (canMoveTo(shape, grid, x + 1, y, rotation)) {
