46041e95e8e5286f90b236629fac1f00fa4e9325
mohamedmehdigara <garamehdi57@yahoo.com>
2023-01-11 08:12:16 +0100

    Added connection for controls

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -1,26 +1,29 @@
 import React from 'react'
+import { moveDown, moveLeft, moveRight, rotate } from '../actions'
 
 export default function Controls(props) {
+    const dispatch = useDispatch()
+    const isRunning = useSelector((state) => state.isRunning)
     return (
         <div className="controls">
             {/* left */}
             <button className="control-button" onClick={(e) => {
-
+                dispatch(moveLeft())
             }}>Left</button>
 
             {/* right */}
             <button className="control-button" onClick={(e) => {
-
+                dispatch(moveRight())
             }}>Right</button>
 
             {/* rotate */}
             <button className="control-button" onClick={(e) => {
-
+                dispatch(rotate())
             }}>Rotate</button>
 
             {/* down */}
             <button className="control-button" onClick={(e) => {
-
+                dispatch(moveDown())
             }}>Down</button>
 
         </div>
